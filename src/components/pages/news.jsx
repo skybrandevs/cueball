24910f421042cfb5ec9fddfef5d9b79e82f81374
johnson chidubem <johnsonchidubem@johnsons-MacBook-Pro.local>
2019-12-26 17:15:24 +0100

    correction

--- src/components/pages/news.jsx
+++ src/components/pages/news.jsx
@@ -24,7 +24,7 @@ const News = () => (
 <div className="col-sm-12">
 <h4 className="leader-font"><img src={newsupdates} className="img-fluid help-game"/>News & Updates</h4>
 
-<h6 className="top-leader-2">CUEBALL LAUNCHES ON DEC.26/19</h6>
+<h6 className="top-leader-2">CUEBALL LAUNCHES ON JAN.3/20</h6>
     <p className="no-data">We are pleased to inform the entire stakeholders that we would be launching on the 26th decemeber 2019.</p>
 
 </div>
